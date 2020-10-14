009ceff6469a44a47171f6c1d5979d5ab8b7dff5
gautam khattri <gautamkhattri07@gmail.com>
2020-10-14 16:10:16 +0530

    views edit

--- Blog/views.py
+++ Blog/views.py
@@ -33,6 +33,7 @@ class UserPostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+    template_name = 'blog/post_detail.html'
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
