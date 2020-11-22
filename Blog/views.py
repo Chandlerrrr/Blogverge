2a767b0e983fe098131893cc7fd9c63b051ad082
gautam khattri <gautamkhattri07@gmail.com>
2020-11-22 15:49:54 +0530

    error

--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from users.forms import CommentForm
@@ -32,9 +32,34 @@ class UserPostListView(ListView):
         return Post.objects.filter(author=user).order_by('-date_posted')
 
 
+def post_detail(request, slug):
+    template_name = 'blog/post_detail.html'
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(request, template_name, {'post': post,
+                                               'comments': comments,
+                                               'new_comment': new_comment,
+                                               'comment_form': comment_form})
+
+
+'''
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
+'''
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
@@ -77,27 +102,3 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
-
-
-def post_detail(request, slug):
-    template_name = 'blog/post_detail.html'
-    post = get_object_or_404(Post, slug=slug)
-    comments = post.comments.filter(active=True)
-    new_comment = None
-    if request.method == 'POST':
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
-            # Create Comment object but don't save to database yet
-            new_comment = comment_form.save(commit=False)
-            # Assign the current post to the comment
-            new_comment.post = post
-            # Save the comment to the database
-            new_comment.save()
-        else:
-            comment_form = CommentForm()
-
-        return render(request, template_name, {'post': post,
-                                               'comments': comments,
-                                               'new_comment': new_comment,
-                                               'comment_form': comment_form})
-
