04c5bf2dcb3d20e2723f40c7bfe59d2189ffa725
gautam khattri <gautamkhattri07@gmail.com>
2020-11-24 01:05:37 +0530

    d14

--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from users.forms import CommentForm
@@ -32,34 +32,35 @@ class UserPostListView(ListView):
         return Post.objects.filter(author=user).order_by('-date_posted')
 
 
-def post_detail(request, id):
-    template_name = 'blog/post_detail.html'
-    post = get_object_or_404(Post, pk=id)
-    comments = post.comments.filter(active=True).order_by("-created_on")
-    new_comment = None
-    # Comment posted
-    if request.method == "POST":
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
-            # Create Comment object but don't save to database yet
-            new_comment = comment_form.save(commit=False)
-            # Assign the current post to the comment
-            new_comment.post = post
-            # Save the comment to the database
-            new_comment.save()
-    else:
-        comment_form = CommentForm()
+class PostDetailView(DetailView):
+    def post_detail(self, request, pk):
+        template_name = 'blog/post_detail.html'
+        post = get_object_or_404(Post, pk)
+        comments = post.comments.filter(active=True).order_by("-created_on")
+        new_comment = None
+        # Comment posted
+        if request.method == "POST":
+            comment_form = CommentForm(data=request.POST)
+            if comment_form.is_valid():
+                # Create Comment object but don't save to database yet
+                new_comment = comment_form.save(commit=False)
+                # Assign the current post to the comment
+                new_comment.post = post
+                # Save the comment to the database
+                new_comment.save()
+        else:
+            comment_form = CommentForm()
 
-    return render(
-        request,
-        template_name,
-        {
-            "post": post,
-            "comments": comments,
-            "new_comment": new_comment,
-            "comment_form": comment_form,
-        },
-    )
+        return render(
+            request,
+            template_name,
+            {
+                "post": post,
+                "comments": comments,
+                "new_comment": new_comment,
+                "comment_form": comment_form,
+            },
+        )
 
 
 # class PostDetailView(DetailView):
