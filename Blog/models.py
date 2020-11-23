90e1fd5a2e1663e041670b20e50608019e1e5d52
gautam khattri <gautamkhattri07@gmail.com>
2020-11-23 17:04:38 +0530

    d5

--- Blog/models.py
+++ Blog/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.template.defaultfilters import slugify
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -10,20 +9,16 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    slug = models.SlugField(
-        verbose_name="Slug",
-        allow_unicode=True,
-        unique=True, default="")
+    slug = models.SlugField(max_length=200, unique=True)
 
-    def save(self, *args, **kwargs):
-        if not self.slug: self.slug = slugify(self.title)
-        super(Post, self).save(*args, **kwargs)
+    class Meta:
+        ordering = ["-date_posted"]
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
+        return reverse("post_detail", kwargs={"slug": str(self.slug)})
 
 
 class Comment(models.Model):
