8695121576b390bee1829afcd09f93a4641c9c72
gautam khattri <gautamkhattri07@gmail.com>
2020-11-24 00:59:42 +0530

    d13

--- Blog/urls.py
+++ Blog/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post-delete"),
     path('about/', views.about, name="blog-about"),
-    path('post/<int:pk>', views.post_detail, name="post-detail"),
+    path('<int:pk>', views.post_detail, name="post-detail"),
 ]
 
 
