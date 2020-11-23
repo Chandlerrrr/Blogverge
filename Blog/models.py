4e4a71ec2d6b11162ea96b4449bca18fdfc5c0b6
gautam khattri <gautamkhattri07@gmail.com>
2020-11-23 22:49:27 +0530

    d8

--- Blog/models.py
+++ Blog/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("post_detail", kwargs={'pk': self.pk})
+        return reverse("post-detail", kwargs={"slug": str(self.slug)})
 
 
 class Comment(models.Model):
