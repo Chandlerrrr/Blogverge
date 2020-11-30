20df722a3951e78e2b9b624926d12d8a963504d8
gautam khattri <gautamkhattri07@gmail.com>
2020-11-30 22:43:53 +0530

    z12

--- Blog/views.py
+++ Blog/views.py
@@ -17,7 +17,14 @@ def home(request):
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+
     return HttpResponseRedirect(reverse('post-detail', args=[str(pk)]))
 
 
