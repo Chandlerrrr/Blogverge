3a0f11ef4c19834995bcd6ac38539a864e8fb441
gautam khattri <gautamkhattri07@gmail.com>
2020-12-03 17:43:01 +0530

    adding reply comment t7

--- Blog/views.py
+++ Blog/views.py
@@ -73,6 +73,47 @@ class PostCommentView(CreateView):
     def get_success_url(self):
         return reverse_lazy('post-detail', kwargs={'pk': self.kwargs['pk']})
 
+    def post_detail(self, request, post, pk):
+        # get post object
+        post = get_object_or_404(Post, pk=id)
+        # list of active parent comments
+        comments = post.comments.filter(active=True, parent__isnull=True)
+        if request.method == 'POST':
+            # comment has been added
+            comment_form = CommentForm(data=request.POST)
+            if comment_form.is_valid():
+                parent_obj = None
+                # get parent comment id from hidden input
+                try:
+                    # id integer e.g. 15
+                    parent_id = int(request.POST.get('parent_id'))
+                except:
+                    parent_id = None
+                # if parent_id has been submitted get parent_obj id
+                if parent_id:
+                    parent_obj = Comment.objects.get(id=parent_id)
+                    # if parent object exist
+                    if parent_obj:
+                        # create replay comment object
+                        replay_comment = comment_form.save(commit=False)
+                        # assign parent_obj to replay comment
+                        replay_comment.parent = parent_obj
+                # normal comment
+                # create comment object but do not save to database
+                new_comment = comment_form.save(commit=False)
+                # assign ship to the comment
+                new_comment.post = post
+                # save
+                new_comment.save()
+                return HttpResponseRedirect(post.get_absolute_url())
+        else:
+            comment_form = CommentForm()
+        return render(request,
+                      'blog/post_detail.html',
+                      {'post': post,
+                       'comments': comments,
+                       'comment_form': comment_form})
+
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
